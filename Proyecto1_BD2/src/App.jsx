b820ab4188de6481e1d1c51a4467d10858beca56
Manuel-Archila <60369044+Manuel-Archila@users.noreply.github.com>
2023-01-30 13:41:32 -0600

    Creacion de proyecto

---
+++ Proyecto1_BD2/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import Login from './views/login/login'
+
+const  App = ()=> {
+  
+
+  return (
+    <>
+      <Login />
+    </>
+  )
+}
+
+export default App
---
+++ Proyecto1_BD2/src/views/login/login.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './login.css'
+import twtitter from '../assets/twitter.svg'
+
+const Login = () => {
+    return (
+        <div className="container">
+            <div className="loginContainer">
+                <img />
+            </div>
+        </div>
+    )
+}
+
+export default Login
