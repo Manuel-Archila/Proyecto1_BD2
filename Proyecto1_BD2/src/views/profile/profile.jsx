c522d6a8680090813524c161305151bd30c4155e
Manuel-Archila <60369044+Manuel-Archila@users.noreply.github.com>
2023-02-02 10:58:10 -0600

    Profile page

--- Proyecto1_BD2/src/views/profile/profile.jsx
+++ Proyecto1_BD2/src/views/profile/profile.jsx
@@ -4,8 +4,10 @@ import twitter from '../../assets/twitter.jpg'
 import LeftMenu from '../../components/LeftMenu.jsx'
 import Profilefeed from '../../components/profile_feed/profile_feed.jsx'
 import TweetDetail from '../tweetDetail/tweetDetail'
+import { useNavigate } from 'react-router-dom'
 
 const Profile = () => {
+    const navigation = useNavigate()
 
     const [feed, setFeed] = useState(true);
 
@@ -16,14 +18,21 @@ const Profile = () => {
             </div>
             <div className="info-container">
                 <div className="back">
-                    <div className="back-arrow"/>
-                    <p>Manuel Archila</p>
+                    <div onClick={() =>{navigation("/home")}} className="back-arrow"/>
+                    <p>Archila15</p>
                 </div>
                 <div className="banner">
-                    <div className="circle"/>
+                    <div className="profpic"/>
                 </div>
                 <div className="info">
-
+                    <div className="name">
+                        <h2>Archila15</h2>
+                    </div>
+                </div>
+                <div className="yours">
+                    <div className='yours-item'>Tweets</div>
+                    <div className='yours-item'>Comments</div>
+                    <div className='yours-item'>Likes</div>
                 </div>
                 <div className="feed-container">
                 {feed ? <Profilefeed clickTweet={() => setFeed(false)}/> : <TweetDetail clickBack={() => setFeed(true)}/>}
