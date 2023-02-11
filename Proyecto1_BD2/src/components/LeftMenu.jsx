4e1fac6c35bcc9ed30da587d1fc36e5180204d7f
Daniel <gon20293@uvg.edu.gt>
2023-02-11 16:49:57 -0600

    delete user

--- Proyecto1_BD2/src/components/LeftMenu.jsx
+++ Proyecto1_BD2/src/components/LeftMenu.jsx
@@ -12,24 +12,54 @@ import { useNavigate } from 'react-router-dom'
 import Switch from '@mui/material/Switch'
 import { alpha, styled } from '@mui/material/styles';
 import { pink } from '@mui/material/colors';
-
-
-const PinkSwitch = styled(Switch)(({ theme }) => ({
-    '& .MuiSwitch-switchBase.Mui-checked': {
-      color: pink[600],
-      '&:hover': {
-        backgroundColor: alpha(pink[600], theme.palette.action.hoverOpacity),
-      },
-    },
-    '& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track': {
-      backgroundColor: pink[600],
-    },
-  }))
+import { useRef, useState } from 'react'
 
 const LeftMenu = ({picture}) => {
     const navigation = useNavigate()
     const user = localStorage.getItem('user')
     const label = { inputProps: { 'aria-label': 'Switch demo' } };
+    const on = useRef(false)
+
+    const onSwitchChange = async () => {
+        on.current = !on.current
+        const URL = 'http://localhost:5000/valentines/'
+        const user = localStorage.getItem('userId')
+        const response = await fetch(URL, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: JSON.stringify({
+                "id": user,
+                "checked": on.current,
+            })
+        })
+
+        const responseJson = await response.json()
+    }
+
+    const deleteHandle = async () => {
+        const URL = 'http://localhost:5000/user/'
+        const userId = localStorage.getItem('userId')
+        const user = localStorage.getItem('user')
+        const response = await fetch(URL, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: JSON.stringify({
+                "id": userId,
+                "user": user
+            })
+        })
+
+        const responseJson = await response.json()
+        if (responseJson.message === "200") [
+            navigation('/')
+        ]
+    }
 
     return (
         <div className="Menu-Container">
@@ -76,16 +106,15 @@ const LeftMenu = ({picture}) => {
             </div>
 
             <div className="Boton-Container2">
-                <div className='toggle-container'>
-                    Día del cariño
-                    <PinkSwitch {...label}  />
-                </div>
                 <div className="perfil-container">
                     <div className="image_profile" style={{ backgroundImage: `url(${picture})` }} />
                     <div className="perfil-p-container">
                         <div className="arroba"> {user}</div>
 
                         </div>
+                </div>
+                <div className='toggle-container'>
+                    <div onClick={deleteHandle} className='delete-account'>Eliminar cuenta</div>
                 </div>
             </div>
 
