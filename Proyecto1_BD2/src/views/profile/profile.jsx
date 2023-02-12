92d22d288364bed157a4726b3e3c3b0706f510d0
Manuel-Archila <60369044+Manuel-Archila@users.noreply.github.com>
2023-02-12 15:08:08 -0600

    Editar la descripcion

--- Proyecto1_BD2/src/views/profile/profile.jsx
+++ Proyecto1_BD2/src/views/profile/profile.jsx
@@ -9,11 +9,44 @@ import { useNavigate } from 'react-router-dom'
 const Profile = () => {
     const navigation = useNavigate()
 
-    const [feed, setFeed] = useState(true);
+    const [feed, setFeed] = useState(true)
     const [desc, setDesc] = useState('')
     const [image, setImage] = useState('')
+    const [edit, setEdit] = useState(false)
 
     const user = localStorage.getItem('user')
+
+    const handleEdit = () => {
+        if (edit === true) {
+            onEdit()
+        }
+        setEdit(!edit)
+    }
+
+    const editDesc = (e) => {
+        if (e.key === e.BACKSPACE) {
+            setDesc(e.target.value)
+        }
+    }
+
+    const onEdit = async () => {
+        const URL = 'http://localhost:5000/description/'
+        const user = localStorage.getItem('user')
+        const response = await fetch(URL, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: JSON.stringify({
+                "username": user,
+                "new_desc": desc,
+            })
+        })
+
+        const responseJson = await response.json()
+        console.log(responseJson)
+    }
 
 
     const getDescription = async() => {
@@ -33,6 +66,8 @@ const Profile = () => {
         getDescription()
     }, [])
 
+
+
     return (
         <div className="container-home">
             <div className="menu-container-home">
@@ -47,8 +82,14 @@ const Profile = () => {
                     <div className="profpic" style={{backgroundImage:`url(${image})`}}/>
                 </div>
                 <div className="info">
-                    <div className="username">{user}</div>
-                    <div className="desc">{desc}</div>
+                    <div className="infotop">
+                        <div className="username">{user}</div>
+                        {!edit ? <p className='peditar' onClick={handleEdit}>Editar</p>
+                            :<p className='peditar' onClick={handleEdit}>Confirmar</p>}
+                    </div>
+                    <div className='descontainer'>
+                        {!edit ? <div className="desc">{desc}</div> : <textarea onChange={editDesc} maxLength="130" className="edit-tweet" >{desc}</textarea>}
+                    </div>
                 </div>
                 <div className="yours">
                     <div className='yours-item'>Tweets</div>
